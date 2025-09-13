7423e16f32d2fb52327c05a8096cdbdf7e952fd0
Kajen Thavaraj <kajenthavaraj@gmail.com>
2025-09-13 04:18:33 -0400

    type script

--- backend/llms.py
+++ backend/llms.py
@@ -1,5 +1,18 @@
 import os
+import cohere
 
-COHERE_API = os.getenv("COHERE_API_KEY")
+COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+
+print(COHERE_API_KEY)
 
 
+# def stream_chat(prompt: str):
+#     co = cohere.Client(COHERE_API_KEY)
+#     response = co.chat_stream(
+#         model="command-a-03-2025",
+#         messages=[{"role": "user", "content": "hello world!"}],
+#     )
+
+#     for event in response:
+#         if event.type == "content-delta":
+#             print(event.delta.message.content.text, end="")
